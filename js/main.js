a63b2e2af9d63bd3fca6173fb72e74fbd14895a2
Victor-Park <jsxpark@yahoo.com>
2022-04-22 22:09:15 +0000

    completed feature-2-race-datA

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var ul = document.querySelector('ul');
 var $li = document.querySelectorAll('li');
 var menuButton = document.querySelector('.menu');
+var tbody = document.querySelector('tbody');
 
 function getYearlyData(year) {
   var xhr = new XMLHttpRequest();
@@ -9,28 +10,38 @@ function getYearlyData(year) {
   xhr.addEventListener('load', function () {
     document.querySelector('.season-header').textContent = xhr.response.MRData.RaceTable.season + ' Season';
     var raceDataArr = xhr.response.MRData.RaceTable.Races;
-    for (var i = 0; i < raceDataArr.length; i++) {
+    for (var j = 0; j < raceDataArr.length; j++) {
       var tr = document.createElement('tr');
-      for (var j = 0; j < raceDataArr.length; j++) {
-        var tbody = document.querySelector('tbody');
-        var td = document.createElement('td');
-        var round = document.createTextNode(raceDataArr[i].round);
-        var raceName = document.createTextNode(raceDataArr[i].raceName);
-        var date = document.createTextNode(raceDataArr[i].date);
-        var time = document.createTextNode(raceDataArr[i].time);
-        var circuit = document.createTextNode(raceDataArr[i].circuitName);
-        td.appendChild(round);
-        td.appendChild(raceName);
-        td.appendChild(date);
-        td.appendChild(time);
-        td.appendChild(circuit);
-        tr.appendChild(td);
-      }
+      tr.setAttribute('class', 'trData');
+      var td1 = document.createElement('td');
+      var td2 = document.createElement('td');
+      var td3 = document.createElement('td');
+      var td4 = document.createElement('td');
+      var td5 = document.createElement('td');
+      var round = document.createTextNode(raceDataArr[j].round);
+      var raceName = document.createTextNode(raceDataArr[j].raceName);
+      var date = document.createTextNode(raceDataArr[j].date);
+      var time = document.createTextNode(raceDataArr[j].time);
+      var circuit = document.createTextNode(raceDataArr[j].Circuit.circuitName);
+      td1.appendChild(round);
+      td2.appendChild(raceName);
+      td3.appendChild(date);
+      td4.appendChild(time);
+      td5.appendChild(circuit);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(td5);
       tbody.appendChild(tr);
     }
   });
   xhr.send();
 }
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  getYearlyData('2022');
+});
 
 ul.addEventListener('click', function (event) {
   if (event.target.matches('li.year')) {
@@ -38,6 +49,10 @@ ul.addEventListener('click', function (event) {
       $li[i].className = 'year light-gray';
     }
     event.target.className = 'year red';
+    var trData = document.querySelectorAll('.trData');
+    for (var j = 0; j < trData.length; j++) {
+      tbody.removeChild(trData[j]);
+    }
     getYearlyData(event.target.textContent);
   }
 });
